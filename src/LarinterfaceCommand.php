c6e9ba26498f3e9b855f1516f98124bec19ac440
nicolasbeauvais <nicolasbeauvais1@gmail.com>
2015-08-12 11:04:18 +0200

    Refactor getClass and generate methods for better code with ReflectionClass workaround

--- src/LarinterfaceCommand.php
+++ src/LarinterfaceCommand.php
@@ -49,30 +49,35 @@ class LarinterfaceCommand extends Command
     {
         $classes = $this->larinterface->getClasses();
 
-        foreach ($classes as $output => $class_array) {
-            foreach ($class_array as $class) {
-                $result = $this->larinterface->generate($class, $output);
+        foreach ($classes as $class => $output) {
 
-                $code = is_array($result) ? $result[0] : $result;
+            $result = $this->larinterface->generate(
+                $class,
+                $output['output'],
+                $output['output_file'],
+                $output['namespace'],
+                $output['name']
+            );
 
-                if ($code === Larinterface::SUCCESS) {
-                    $msg = '[SUCCESS] ' . $class;
+            $code = is_array($result) ? $result[0] : $result;
 
-                    if ($result[1] > 0) {
-                        $msg .= ' [MISSING: ' . $result[1] . ' comment block]';
-                    }
+            if ($code === Larinterface::SUCCESS) {
+                $msg = '[SUCCESS] ' . $class;
 
-                    $this->info($msg);
-                } elseif ($code === Larinterface::EMPTY_CLASS) {
-                    $this->comment('No method in class ' . $class . ' to generate an Interface');
-                } elseif ($code === Larinterface::NOT_CLASS) {
-                    $this->comment('[IGNORED] ' . $class);
-                } elseif ($code === Larinterface::NO_MODIFICATION) {
-                    // Do nothing
-                } else {
-                    $this->error('[ERROR]   Can\'t write file: ' . $result[1]);
+                if ($result[1] > 0) {
+                    $msg .= ' [MISSING: ' . $result[1] . ' comment block]';
                 }
-            }
+
+                $this->info($msg);
+            } elseif ($code === Larinterface::EMPTY_CLASS) {
+                $this->comment('No method in class ' . $class . ' to generate an Interface');
+            } elseif ($code === Larinterface::NOT_CLASS) {
+                $this->comment('[IGNORED] ' . $class);
+            } elseif ($code === Larinterface::NO_MODIFICATION) {
+                // Do nothing
+            } else {
+                $this->error('[ERROR]   Can\'t write file: ' . $result[1]);
+                }
         }
     }
 }
